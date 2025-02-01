8bd77b3f44103d1f46fd66d5028b388863865440
kapibaradeioi <rachellrpa10@gmail.com>
2025-01-31 23:21:03 -0500

    1.3

--- obj/Debug/net8.0/prueba.AssemblyInfo.cs
+++ obj/Debug/net8.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77a7c136e77bd83f564ed0062d24989182fcb1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4035ef6a3cb1c76ea90956342009c6d0ea4f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
